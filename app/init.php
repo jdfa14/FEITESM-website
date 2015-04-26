4dee9df453bec7df8bcf31945482dd77fb1a2764
Jesus David De La Fuente Amaya <jdfa14@gmail.com>
2015-04-26 01:19:23 -0500

    Google API Spreadsheet
    Anadidos documentos para el uso de Google Spreadsheet API

--- app/init.php
+++ app/init.php
@@ -3,5 +3,6 @@
 session_start();
 
 require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 require_once 'classes/Database.php';
 require_once 'classes/Google_Auth.php';
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Google\\' => array($vendorDir . '/asimlqt/php-google-spreadsheet-client/src/Google'),
 );
